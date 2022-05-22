5e959ade07319b807fcb144b1cc92df9f6743e42
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-22 14:56:52 +0530

    hero editor right section

--- components/sections/SectionEditor.js
+++ components/sections/SectionEditor.js
@@ -31,7 +31,7 @@ const SectionEditor = () => {
         </label>
       </div>
 
-      <div className="relative flex justify-center items-end w-[130vw] left-[-15vw]">
+      <div className="relative flex justify-center items-end w-[130vw]">
         <div className="max-w-md flex flex-col justify-end items-end mr-6 w-1/6 md:w-4/12">
           <div className="h-[1px] pt-[20%] mt-[20%] mb-4 w-1/4 shadow-overlay-card opacity-75 after:shadow-overlay-card-after" />
           <div className="hidden md:flex justify-end items-center mb-4 w-full">
@@ -84,6 +84,57 @@ const SectionEditor = () => {
             height="22"
             style={{ width: '180%', left: '-40%', top: '-60%' }}
           />
+        </div>
+        <div className="max-w-md flex flex-col justify-start items-start ml-6 w-1/6 md:w-4/12">
+          <div className="h-[1px] pb-[30%] mt-[5%] mb-4 w-1/6 shadow-overlay-card opacity-75 after:shadow-overlay-card-after" />
+          <div className="hidden md:flex justify-start items-end mb-4 w-full">
+            <div className="p-6 shadow-overlay-card h-32 text-gh-muted mr-4">
+              <p>Memory up to</p>
+              <h3 className="text-5xl font-extrabold text-gradient-blue-purple">
+                64
+                <span className="text-2xl font-bold ml-2">GB</span>
+              </h3>
+            </div>
+            <div className="relative block h-20 w-1/3 shadow-overlay-card opacity-75 after:shadow-overlay-card-after" />
+          </div>
+          <div className="p-2 shadow-overlay-card  mb-4 after:shadow-overlay-card-after w-7/12">
+            <Image
+              src="/assets/editor/illo-hero-branches.png"
+              alt="cpu"
+              width={247}
+              height={181}
+              className="block"
+            />
+          </div>
+          <div className="flex gap-4">
+            <div className="p-2 md:p-6 shadow-overlay-card after:shadow-overlay-card-after w-7/12">
+              <Image
+                src="/assets/editor/illo-hero-commits.png"
+                alt="cpu"
+                width={32}
+                height={32}
+                className="block"
+              />
+            </div>
+            <div className="p-2 md:p-6 shadow-overlay-card after:shadow-overlay-card-after w-7/12">
+              <Image
+                src="/assets/editor/illo-hero-vm.png"
+                alt="cpu"
+                width={32}
+                height={32}
+                className="block"
+              />
+            </div>
+            <div className="p-2 md:p-6 shadow-overlay-card after:shadow-overlay-card-after w-7/12">
+              <Image
+                src="/assets/editor/illo-hero-checks.png"
+                alt="cpu"
+                width={32}
+                height={32}
+                className="block"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
