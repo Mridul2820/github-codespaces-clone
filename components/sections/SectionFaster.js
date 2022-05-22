1f5a6393a625c7e0b0612377f44b2ab03f25fb34
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-22 22:44:30 +0530

    fast section borrom items

--- components/sections/SectionFaster.js
+++ components/sections/SectionFaster.js
@@ -4,10 +4,20 @@ const SectionFaster = ({ data }) => {
   return (
     <div className="my-16 pb-16 mx-auto max-w-7xl h-auto">
       <div className="-mx-6 flex items-center">
-        <div className="px-6 py-4 w-1/2">
+        <div className="px-6 py-4 w-1/2 -mt-[7%]">
           <div className="pr-5">
             <h3 className="mb-4 text-5xl font-extrabold ">{data.title}</h3>
             <p className="text-gh-secondary text-xl mb-5">{data.description}</p>
+            <div className="mt-10 flex w-full">
+              {data.stats?.map((stat, index) => (
+                <div key={index} className="w-1/3">
+                  <p className="text-gh-secondary">
+                    {stat.detail}
+                  </p>
+                  <p className="font-extrabold text-3xl">{stat.stat}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
 
