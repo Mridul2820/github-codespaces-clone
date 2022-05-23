b821daa3d6a8e7e062f60c507ae76467b26d2663
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-24 00:53:57 +0530

    config update

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ module.exports = {
     formats: ['image/avif', 'image/webp'],
     domains: ['localhost', 'res.cloudinary.com'],
   },
-  exportPathMap: function () {
-    return {
-      '/locales/de-DE/summer-dress-f': {
-        page: '/locales/[locale]/[slug]',
-        query: { locale: 'de-DE', slug: 'summer-dress-f' },
-      },
-    };
-  },
 
   webpack: (config) => {
     config.resolve.alias['components'] = path.join(__dirname, 'components');
