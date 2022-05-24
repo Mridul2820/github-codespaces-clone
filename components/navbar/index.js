8d74b9de3b3cb0c86c34ab3208d7c69693f2ed83
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-24 11:30:01 +0530

    nar bar updates

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
 import { BsGithub } from 'react-icons/bs';
 
 const index = ({ headerData }) => {
+  console.log(headerData);
   return (
-    <div className="flex py-3 px-8 bg-gh-primary">
-      <BsGithub size={32} color="white" />
+    <div className="flex py-4 px-8 bg-gh-primary">
+      <div className="flex items-center gap-4">
+        <BsGithub size={32} color="white" />
+        <form className="relative w-[272px]">
+          <input
+            className="gh-search-input"
+            placeholder="Search or jump to…"
+            type="text"
+          />
+          <span className='gh-search-input-icon'>/</span>
+        </form>
+        {headerData.mainlinks.map((link, index) => (
+          <Link href={link.href} key={index}>
+            <a className="text-sm font-semibold text-white">{link.text}</a>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
